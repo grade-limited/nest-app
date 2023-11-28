b56ba5030d9e32a3c75f050294130d39f98343ee
tam11a <ibrahimsadiktamim@gmail.com>
2023-11-29 01:06:22 +0600

    file upload done

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -11,6 +11,21 @@ import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { UploadSingleFileDto } from './dto/upload-single-file';
 import { UploadMultipleFilesDto } from './dto/upload-multiple-files';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+const storage = diskStorage({
+  destination: process.env.FILE_UPLOAD_PATH || '../file_bucket',
+  filename: (_req, file, cb) => {
+    const name = file.originalname.split('.')[0]?.toLowerCase();
+    const extension = extname(file.originalname);
+    const randomName = Array(32)
+      .fill(null)
+      .map(() => Math.round(Math.random() * 16).toString(16))
+      .join('');
+    cb(null, `${name}-${randomName}${Date.now()}${extension}`);
+  },
+});
 
 @ApiTags('Files')
 @Controller('files')
@@ -18,7 +33,7 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('upload/single')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', { storage }))
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     type: UploadSingleFileDto,
@@ -29,7 +44,7 @@ export class FilesController {
   }
 
   @Post('upload/multiple')
-  @UseInterceptors(FilesInterceptor('files', 10))
+  @UseInterceptors(FilesInterceptor('files', 10, { storage }))
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     type: UploadMultipleFilesDto,
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,12 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class FilesService {
   async uploadSingleFile(file: Express.Multer.File) {
-    console.log(file);
-    return 'data is here';
+    return file;
   }
 
   async uploadMultipleFiles(files: Express.Multer.File[]) {
-    console.log(files);
-    return 'data is here';
+    return files;
   }
 }
