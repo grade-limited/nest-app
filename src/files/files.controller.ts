b25f08fcb279db420440f029ba9f04c2113e9a4e
tam11a <ibrahimsadiktamim@gmail.com>
2023-11-29 01:36:02 +0600

    added: files.storage.ts file

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -11,21 +11,7 @@ import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { UploadSingleFileDto } from './dto/upload-single-file';
 import { UploadMultipleFilesDto } from './dto/upload-multiple-files';
-import { diskStorage } from 'multer';
-import { extname } from 'path';
-
-const storage = diskStorage({
-  destination: process.env.FILE_UPLOAD_PATH || '../file_bucket',
-  filename: (_req, file, cb) => {
-    const name = file.originalname.split('.')[0]?.toLowerCase();
-    const extension = extname(file.originalname);
-    const randomName = Array(32)
-      .fill(null)
-      .map(() => Math.round(Math.random() * 16).toString(16))
-      .join('');
-    cb(null, `${name}-${randomName}${Date.now()}${extension}`);
-  },
-});
+import storage from './files.storage';
 
 @ApiTags('Files')
 @Controller('files')
