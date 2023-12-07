207daa2d082243694513f173459f2aabb58e6af6
Moon2340 <farjanamoon20144@gmail.com>
2023-12-07 14:43:09 +0600

    shop

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ShopService } from './shop.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Shop')
 @Controller('shop')
 export class ShopController {
   constructor(private readonly shopService: ShopService) {}
+  @Get('product/:id')
+  findOne(@Param('id') id: string) {
+    return this.shopService.findOne(+id);
+  }
 }
--- src/shop/shop.service.ts
+++ src/shop/shop.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import Product from 'src/products/entities/product.entity';
 
 @Injectable()
-export class ShopService {}
+export class ShopService {
+  async findOne(id: number) {
+    const product = await Product.findByPk(id, {
+      include: [
+        {
+          association: 'brand',
+        },
+        {
+          association: 'category',
+        },
+      ],
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product not found`);
+    }
+    return {
+      message: 'Product fetched successfully',
+      data: product,
+    };
+  }
+}
