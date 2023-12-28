1c3bbcde4347cb5e9d235bae61374318a6b675dd
Moon2340 <farjanamoon20144@gmail.com>
2023-12-28 22:44:12 +0600

    changes in register from

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -31,7 +31,7 @@ export class RegisterDto {
   primary_contact: string;
 
   @ApiProperty({
-    enum: ['API', 'Website', 'Android', 'iOS'],
+    enum: ['API', 'Website', 'Android', 'iOS', 'Admin'],
   })
   registered_from: string;
 
